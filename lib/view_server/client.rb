3bb9ca012f845c0977106d24f779a54935c2e3cb
Martin Chabot <chabotm@gmail.com>
2013-11-23 22:45:52 -0500

    Adding delegate from client to server

--- lib/view_server/client.rb
+++ lib/view_server/client.rb
@@ -1,12 +1,17 @@
 require 'drb'
+require 'delegate'
 
 module ViewServer
-  class Client
+  class Client < Delegator
     attr_reader :server
 
     def initialize(port = 10021)
       DRb.start_service
       @server = DRbObject.new(nil, "druby://localhost:#{port}")
+    end
+
+    def __getobj__
+      @server
     end
   end
 end

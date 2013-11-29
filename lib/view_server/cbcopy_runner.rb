97f5a5c620926296dedc307387d0bde275ae5c00
Martin Chabot <chabotm@gmail.com>
2013-11-29 00:19:44 -0500

    Fix usage message for cbcopy

--- lib/view_server/cbcopy_runner.rb
+++ lib/view_server/cbcopy_runner.rb
@@ -6,7 +6,7 @@ module ViewServer
     def self.parse(args)
       options = {:port => 10021}
       opt_parser = OptionParser.new do |opts|
-        opts.banner = "Usage: show [options] [file]"
+        opts.banner = "Usage: cbcopy [options]"
 
         opts.on("-p", "--port PORT", "Port number. Default is 10021") do |v|
           options[:port] = Integer(v)

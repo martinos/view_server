57b7e72db1fbabadefe478b187875f141d5d0e8c
Martin Chabot <chabotm@gmail.com>
2014-02-20 11:24:36 -0500

    Adding debugging info

--- lib/view_server/my_clipboard.rb
+++ lib/view_server/my_clipboard.rb
@@ -3,12 +3,10 @@ require 'clipboard'
 module ViewServer
   class MyClipboard
     def self.copy(data)
-      puts "Copy called"
       Clipboard.copy(data)
     end
 
     def self.paste
-      puts "Paste called"
       Clipboard.paste
     end
   end
